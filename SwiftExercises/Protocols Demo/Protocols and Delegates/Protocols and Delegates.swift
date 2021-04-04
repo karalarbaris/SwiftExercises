032588a0de4012c55e40c2fbcd070bd63dcc8ce2
Baris <karalarbaris@gmail.com>
2021-04-04 21:54:23 +0200

    147. An Example of Protocols and Delegates in Practice

---
+++ SwiftExercises/Protocols Demo/Protocols and Delegates/Protocols and Delegates.swift
@@ -0,0 +1,67 @@
+
+protocol AdvancedLifeSupport {
+    
+    func performCPR()
+
+}
+
+class EmergencyCallHandler {
+    var delegate: AdvancedLifeSupport?
+    
+    func assessSituation() {
+        print("can you tell me what happened?")
+    }
+    
+    func medicalEmergency() {
+        delegate?.performCPR()
+    }
+}
+
+struct Paramedic: AdvancedLifeSupport {
+    
+    init(handler: EmergencyCallHandler) {
+        handler.delegate = self
+    }
+    
+    func performCPR() {
+        print("Paramedic performs CPR")
+    }
+    
+}
+
+class Doctor: AdvancedLifeSupport {
+    
+    init(handler: EmergencyCallHandler) {
+        handler.delegate = self
+    }
+    
+    func performCPR() {
+        print("Doctor performs CPR")
+    }
+    
+    func useStetescope() {
+        print("Doctor uses stetescope")
+    }
+}
+
+class Surgeon: Doctor {
+    override func performCPR() {
+        super.performCPR()
+        print("Surgeon performs CPR")
+    }
+    
+    func useElectricDrill() {
+        print("Surgeon uses electric drill")
+    }
+}
+
+
+//var emilio = EmergencyCallHandler()
+////var pete = Paramedic(handler: emilio)
+////var hans = Doctor(handler: emilio)
+//var baris = Surgeon(handler: emilio)
+//baris.useElectricDrill()
+//
+//emilio.assessSituation()
+//emilio.medicalEmergency()
+
--- SwiftExercises/main.swift
+++ SwiftExercises/main.swift
@@ -1,10 +1,1 @@
 
-
-var emilio = EmergencyCallHandler()
-//var pete = Paramedic(handler: emilio)
-//var hans = Doctor(handler: emilio)
-var baris = Surgeon(handler: emilio)
-baris.useElectricDrill()
-
-emilio.assessSituation()
-emilio.medicalEmergency()
