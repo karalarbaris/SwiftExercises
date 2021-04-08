359fcb500b237b39f5e99d9f4504b69ce45d9d24
Baris <karalarbaris@gmail.com>
2021-04-09 01:53:05 +0200

    157. [Swift Deep Dive] Extensions and Default Implementations for Protocols

---
+++ SwiftExercises/Extensions/Extension for Protocol.swift
@@ -0,0 +1,29 @@
+//protocol CanFly {
+//    func fly()
+//}
+//
+//extension CanFly {
+//    func fly() {
+//        print("Default fly method, with this extension it will not give an error if we don't provide any fly func")
+//    }
+//}
+//
+//struct Airplane: CanFly {
+//    func fly() {
+//        print("Airplane flies")
+//    }
+//
+//
+//}
+//
+//struct Airplane2: CanFly {
+//
+//
+//}
+//
+//
+//let myAirplane = Airplane()
+//myAirplane.fly()
+//
+//let myAirplane2 = Airplane2()
+//myAirplane2.fly()
---
+++ SwiftExercises/Extensions/Extensions.swift
@@ -0,0 +1,22 @@
+//import Foundation
+//
+//extension Double {
+//
+//    func round(to places: Int) -> Double {
+//        let precisionNumber = pow(10, Double(places))
+//        var n = self
+//
+//        n*=precisionNumber
+//        n.round()
+//        n/=precisionNumber
+//        return n
+//    }
+//}
+//
+//let myDouble = 3.14159
+//
+//let myRoundedDouble = String(format: "%0.1f", myDouble)
+//
+//let x = myDouble.round(to: 4)
+//
+//print(x)
