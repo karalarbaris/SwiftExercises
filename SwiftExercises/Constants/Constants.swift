0dec40aa44027f69571b01c59dfb372b92121e76
Baris <karalarbaris@gmail.com>
2021-04-19 16:32:57 +0200

    Constants

---
+++ SwiftExercises/Constants/Constants.swift
@@ -0,0 +1,34 @@
+////
+////  Constants.swift
+////  SwiftExercises
+////
+////  Created by Baris Karalar on 19.04.2021.
+////
+//
+//import Foundation
+//
+//struct MyStructure {
+//    let instanceProperty = "asdf"
+//    static let typeProperty = "1234"
+//    
+//    func instanceMethod() {
+//        print("Instance method")
+//    }
+//    
+//    static func typeMethod() {
+//        print("Type method")
+//    }
+//}
+//
+//let myStructure = MyStructure()
+//print(myStructure.instanceProperty)
+//myStructure.instanceMethod()
+//
+//print(MyStructure.typeProperty)
+//MyStructure.typeMethod()
+//
+//
+//
+//
+//
+//
