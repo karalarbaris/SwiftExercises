107deda83e5ded02cc9b0f74bc926833fffe4765
Baris <karalarbaris@gmail.com>
2021-05-11 14:12:37 +0200

    299. Advanced Properties Challenge

---
+++ SwiftExercises/Computed Properties/Example.swift
@@ -0,0 +1,22 @@
+//import Foundation
+//
+//var width: Float = 1.5
+//var height: Float = 2.3
+//
+//
+//var bucketsOfPaint : Int {
+//    get {
+//        let area = width * height
+//        let areaCoveredPerBucket : Float = 1.5
+//        return Int((area / areaCoveredPerBucket).rounded(.up))
+//    }
+//    
+//    set {
+//        let area = Double(newValue) * 1.5
+//        print("\(bucketsOfPaint) amount of buckets can cover \(area) ")
+//    }
+//}
+//
+//print(bucketsOfPaint)
+//
+//bucketsOfPaint = 14
