fdbc916e401964f752f10f7ee05bc7bb872b1db1
Baris <karalarbaris@gmail.com>
2021-05-13 14:00:32 +0200

    Tuples

---
+++ SwiftExercises/Tuples/Tuples.swift
@@ -0,0 +1,8 @@
+//
+//  Tuples.swift
+//  SwiftExercises
+//
+//  Created by Baris Karalar on 13.05.2021.
+//
+
+import Foundation
--- SwiftExercises/main.swift
+++ SwiftExercises/main.swift
@@ -1,1 +1,17 @@
 
+// This is not a preferred way to create tuple
+let tuple1 = ("baris", 12)
+
+print(tuple1.0)
+
+let tuple2 = (name: "Baris", age: 12)
+
+print(tuple2.age)
+
+var tuple3 : (name: String, age: Int)
+tuple3.name = "baris"
+tuple3.age = 12
+// or
+tuple3 = (name: "asdsdf", age: 88)
+
+print(tuple3.name)
