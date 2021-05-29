1ea3eb68bf361f2b68126b1a4b5c99bc3a4bc3ff
Baris <karalarbaris@gmail.com>
2021-05-29 08:32:32 +0200

    Tuples

--- SwiftExercises/Tuples/Tuples.swift
+++ SwiftExercises/Tuples/Tuples.swift
@@ -1,8 +1,18 @@
 //
-//  Tuples.swift
-//  SwiftExercises
+//// This is not a preferred way to create tuple
+//let tuple1 = ("baris", 12)
 //
-//  Created by Baris Karalar on 13.05.2021.
+//print(tuple1.0)
 //
-
-import Foundation
+//let tuple2 = (name: "Baris", age: 12)
+//
+//print(tuple2.age)
+//
+//var tuple3 : (name: String, age: Int)
+//tuple3.name = "baris"
+//tuple3.age = 12
+//// or
+//tuple3 = (name: "asdsdf", age: 88)
+//
+//print(tuple3.name)
+//
